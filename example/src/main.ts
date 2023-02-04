5cc35091fe35f7ca5c56a1d9aa6166dcb9fce496
Ubugeeei <ubuge1122@gamil.com>
2023-02-04 18:47:18 +0900

    feat: Vue class

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+import { createApp } from "~/src/index";
+
+const App = createApp({
+	data() {
+		return {
+			message: "Hello chibi-vue!",
+		};
+	},
+});
+
+App.mount("#app");
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello World");
+type ComponentOption = {
+	data(): Record<string, unknown>;
+};
+
+export const createApp = (options: ComponentOption): Vue => {
+	return new Vue(options);
+};
+
+class Vue {
+	$options: ComponentOption;
+	_data: Record<string, unknown>;
+
+	constructor(options: ComponentOption) {
+		this.$options = options;
+		this._data = options.data();
+		console.log(this._data);
+	}
+
+	mount(selector: string) {
+		const root = document.querySelector(selector)!;
+		root.innerHTML = this._data.message as string;
+	}
+}
