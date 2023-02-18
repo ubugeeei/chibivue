1ae91501d464e788064e80b3480196375c66b7f6
Ubugeeei <ubuge1122@gamil.com>
2023-02-18 19:46:44 +0900

    feat: node ops interface

--- src/core/vdom/patch.ts
+++ src/core/vdom/patch.ts
@@ -1,7 +1,22 @@
 import { VNode } from "./vnode";
 
+interface NodeOps {
+  createElement(tagName: string): Element;
+  createTextNode(text: string): Text;
+  createComment(text: string): Comment;
+  insertBefore(parentNode: Node, newNode: Node, referenceNode: Node): void;
+  removeChild(node: Node, child: Node): void;
+  appendChild(node: Node, child: Node): void;
+  parentNode(node: Node): void;
+  nextSibling(node: Node): void;
+  tagName(node: Element): string;
+  setTextContent(node: Node, text: string): void;
+}
+
 export function createPatchFunction({
-  nodeOps: any,
+  nodeOps,
+}: {
+  nodeOps: NodeOps;
 }): (a: Element, b: VNode, parentElm?: any, refElm?: any) => Element {
   // TODO:
   return () => {};
