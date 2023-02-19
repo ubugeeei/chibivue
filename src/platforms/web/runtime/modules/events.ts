d30b40b7478bfaa3de31b5f11ca37c777215f143
Ubugeeei <ubuge1122@gamil.com>
2023-02-20 00:31:47 +0900

    feat: event module

---
+++ src/platforms/web/runtime/modules/events.ts
@@ -0,0 +1,23 @@
+import { VNode } from "~/src/core/vdom/vnode";
+import { isUndef } from "~/src/shared/utils";
+
+let target: any;
+
+function updateDOMListeners(oldVnode: VNode, vnode: VNode) {
+  if (isUndef(oldVnode.data!.on) && isUndef(vnode.data!.on)) {
+    return;
+  }
+  const on = vnode.data?.on || {};
+  const oldOn = oldVnode.data?.on || {};
+  // vnode is empty when removing all listeners,
+  // and use old vnode dom element
+  target = vnode.elm || oldVnode.elm;
+  // TODO:
+  // updateListeners(on, oldOn, add, remove, createOnceHandler, vnode.context);
+  target = undefined;
+}
+
+export default {
+  create: updateDOMListeners,
+  update: updateDOMListeners,
+};
