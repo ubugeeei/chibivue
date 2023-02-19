7e2daa93fbee9a53d9177e0be9fc67b787d242b4
Ubugeeei <ubuge1122@gamil.com>
2023-02-19 17:55:47 +0900

    feat: utils

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,5 +1,36 @@
 export const isArray = Array.isArray;
 
+// These helpers produce better VM code in JS engines due to their
+// explicitness and function inlining.
+export function isUndef(v: any): v is undefined | null {
+  return v === undefined || v === null;
+}
+
+export function isDef<T>(v: T): v is NonNullable<T> {
+  return v !== undefined && v !== null;
+}
+
+export function isTrue(v: any): boolean {
+  return v === true;
+}
+
+export function isFalse(v: any): boolean {
+  return v === false;
+}
+
+/**
+ * Check if value is primitive.
+ */
+export function isPrimitive(value: unknown): boolean {
+  return (
+    typeof value === "string" ||
+    typeof value === "number" ||
+    // $flow-disable-line
+    typeof value === "symbol" ||
+    typeof value === "boolean"
+  );
+}
+
 export function isPlainObject(obj: any): boolean {
   return Object.prototype.toString.call(obj) === "[object Object]";
 }
