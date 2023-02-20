18ff36becadba7c8285b055c5603fe9b2cce09ff
Ubugeeei <ubuge1122@gamil.com>
2023-02-20 23:08:10 +0900

    exmaple: vue render

---
+++ example/vue_render/src/main.ts
@@ -0,0 +1,32 @@
+import { createApp, h } from "vue";
+
+const app = createApp({
+  data() {
+    return {
+      message: "Hello chibi-vue!",
+      count: 0,
+    };
+  },
+  methods: {
+    changeMessage() {
+      this.count++;
+      this.message = `Hello chibi-vue! ${this.count} * 2 = ${this.countDouble}`;
+    },
+  },
+  computed: {
+    countDouble() {
+      return this.count * 2;
+    },
+  },
+  render() {
+    return h(
+      "div",
+      {
+        onClick: this.changeMessage,
+      },
+      this.message
+    );
+  },
+});
+
+app.mount("#app");
