394bebaa23a442f470f32af12c4081548fd99da0
Ubugeeei <ubuge1122@gamil.com>
2023-02-23 17:48:03 +0900

    adjust: renderer option

--- packages/runtime-dom/nodeOps.ts
+++ packages/runtime-dom/nodeOps.ts
@@ -5,7 +5,7 @@ export const nodeOps: RendererOptions = {
     return document.createElement(tagName);
   },
 
-  createTextNode: (text: string): Text => {
+  createText: (text: string): Text => {
     return document.createTextNode(text);
   },
 
@@ -13,16 +13,20 @@ export const nodeOps: RendererOptions = {
     return document.createComment(text);
   },
 
-  insertBefore: (parentNode: Node, newNode: Node, referenceNode: Node) => {
+  setText: (node, text) => {
+    node.nodeValue = text;
+  },
+
+  setElementText: (el, text) => {
+    el.textContent = text;
+  },
+
+  insert: (parentNode: Node, newNode: Node, referenceNode: Node) => {
     parentNode.insertBefore(newNode, referenceNode);
   },
 
-  removeChild: (node: Node, child: Node) => {
+  remove: (node: Node, child: Node) => {
     node.removeChild(child);
-  },
-
-  appendChild: (node: Node, child: Node) => {
-    node.appendChild(child);
   },
 
   parentNode: (node: Node) => {
@@ -31,13 +35,5 @@ export const nodeOps: RendererOptions = {
 
   nextSibling: (node: Node) => {
     return node.nextSibling;
-  },
-
-  tagName: (node: Element): string => {
-    return node.tagName;
-  },
-
-  setTextContent: (node: Node, text: string) => {
-    node.textContent = text;
   },
 };
