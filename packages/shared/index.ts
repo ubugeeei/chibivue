fb13abd2ed5831b019279f7bbec2b62b0b17622e
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 01:35:51 +0900

    feat: add shared helper

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,3 +1,5 @@
+export * from "./toDisplayString";
+
 const onRE = /^on[^a-z]/;
 export const isOn = (key: string) => onRE.test(key);
 
@@ -8,6 +10,11 @@ export const hasOwn = (
 ): key is keyof typeof val => hasOwnProperty.call(val, key);
 
 export const isArray = Array.isArray;
+export const isMap = (val: unknown): val is Map<any, any> =>
+  toTypeString(val) === "[object Map]";
+export const isSet = (val: unknown): val is Set<any> =>
+  toTypeString(val) === "[object Set]";
+
 export const isFunction = (val: unknown): val is Function =>
   typeof val === "function";
 export const isString = (val: unknown): val is string =>
@@ -16,3 +23,16 @@ export const isSymbol = (val: unknown): val is symbol =>
   typeof val === "symbol";
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === "object";
+
+export const objectToString = Object.prototype.toString;
+export const toTypeString = (value: unknown): string =>
+  objectToString.call(value);
+
+export const isPlainObject = (val: unknown): val is object =>
+  toTypeString(val) === "[object Object]";
+
+export const capitalize = (str: string) =>
+  str.charAt(0).toUpperCase() + str.slice(1);
+
+export const toHandlerKey = (str: string) =>
+  str ? `on${capitalize(str)}` : ``;
