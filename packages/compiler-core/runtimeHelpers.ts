c49773ed073864a2b6508c0b72fe22daca21f45c
Ubugeeei <ubuge1122@gamil.com>
2023-03-11 14:06:48 +0900

    feawt: runtimeHelpers

---
+++ packages/compiler-core/runtimeHelpers.ts
@@ -0,0 +1,9 @@
+export const CREATE_ELEMENT_VNODE = Symbol();
+export const WITH_DIRECTIVES = Symbol();
+export const TO_DISPLAY_STRING = Symbol();
+
+export const helperNameMap: Record<symbol, string> = {
+  [CREATE_ELEMENT_VNODE]: `createElementVNode`,
+  [WITH_DIRECTIVES]: `withDirectives`,
+  [TO_DISPLAY_STRING]: `toDisplayString`,
+};
