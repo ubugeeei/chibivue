6e7e65a960d386578371b95e34b68fa2bb76aeee
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 01:33:53 +0900

    feat: toDisplayString

---
+++ packages/shared/toDisplayString.ts
@@ -0,0 +1,43 @@
+import {
+  isArray,
+  isFunction,
+  isMap,
+  isObject,
+  isPlainObject,
+  isSet,
+  isString,
+  objectToString,
+} from ".";
+
+export const toDisplayString = (val: unknown): string => {
+  return isString(val)
+    ? val
+    : val == null
+    ? ""
+    : isArray(val) ||
+      (isObject(val) &&
+        (val.toString === objectToString || !isFunction(val.toString)))
+    ? JSON.stringify(val, replacer, 2)
+    : String(val);
+};
+
+const replacer = (_key: string, val: any): any => {
+  // can't use isRef here since @vue/shared has no deps
+  if (val && val.__v_isRef) {
+    return replacer(_key, val.value);
+  } else if (isMap(val)) {
+    return {
+      [`Map(${val.size})`]: [...val.entries()].reduce((entries, [key, val]) => {
+        (entries as any)[`${key} =>`] = val;
+        return entries;
+      }, {}),
+    };
+  } else if (isSet(val)) {
+    return {
+      [`Set(${val.size})`]: [...val.values()],
+    };
+  } else if (isObject(val) && !isArray(val) && !isPlainObject(val)) {
+    return String(val);
+  }
+  return val;
+};
