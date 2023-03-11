e23cc0114b1a54b50a8d974186f9bdf7f944ff2a
Ubugeeei <ubuge1122@gamil.com>
2023-03-11 23:54:20 +0900

    feat: isSymbol

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -12,5 +12,7 @@ export const isFunction = (val: unknown): val is Function =>
   typeof val === "function";
 export const isString = (val: unknown): val is string =>
   typeof val === "string";
+export const isSymbol = (val: unknown): val is symbol =>
+  typeof val === "symbol";
 export const isObject = (val: unknown): val is Record<any, any> =>
   val !== null && typeof val === "object";
