e2a730d1c1f95bf57148ef576b1f4b0490f07ab7
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 18:49:03 +0900

    test: config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["./packages/**/*.ts"],
+    watch: false,
+    include: ["./packages/reactivity/*.ts", "./packages/shared/*.ts"],
+    exclude: [
+      "./packages/reactivity/index.ts",
+      "./packages/reactivity/dep.ts",
+      "./packages/reactivity/effectScope.ts",
+      "./packages/shared/shapeFlags.ts",
+    ],
   },
 });
