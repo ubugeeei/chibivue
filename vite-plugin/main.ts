4d29edd4c05678e7ede6be3980c500ad36a6a4fc
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 22:52:03 +0900

    wip: resolve code

--- vite-plugin/main.ts
+++ vite-plugin/main.ts
@@ -11,22 +11,12 @@ export async function transformMain(
   const { descriptor } = createDescriptor(filename, code, options);
 
   // TODO: parse and compile
-  return {
-    code: `
-      import { ref, computed } from "chibi-vue";
-      export default {
-        template: \`<div @click="changeMessage">{{ message }}</div>\`,
-        setup() {
-          const count = ref(0);
-          const countDouble = computed(() => count.value * 2);
-          const message = ref("Hello chibi-vue!");
-          const changeMessage = () => {
-            count.value++;
-            message.value = \`Hello chibi-vue! \${count.value} * 2 = \${countDouble.value}\`;
-          };
-          return { message, changeMessage };
-        },
-      }
-    `,
-  };
+  const resolvedCode = descriptor.script?.content.replace(
+    "export default {",
+    `export default {\n  template: \`${
+      descriptor.template?.content.trim() ?? ""
+    }\`,`
+  );
+
+  return { code: resolvedCode };
 }
