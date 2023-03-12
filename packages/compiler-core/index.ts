185278109e042f9033687568091d558b11156e93
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 22:27:58 +0900

    feat: compiler dom parser

--- packages/compiler-core/index.ts
+++ packages/compiler-core/index.ts
@@ -1,3 +1,7 @@
 export { baseCompile } from "./compile";
 
 export * from "./ast";
+export * from "./options";
+export * from "./transform";
+export * from "./codegen";
+export * from "./compile";
--- packages/compiler-dom/index.ts
+++ packages/compiler-dom/index.ts
@@ -1,6 +1,11 @@
-import { baseCompile } from "../compiler-core";
+import { RootNode, baseCompile } from "../compiler-core";
+import { baseParse } from "../compiler-core/parse";
 import { CodegenResult } from "./codegen";
 
 export function compile(template: string): CodegenResult {
   return baseCompile(template) as any;
+}
+
+export function parse(template: string): RootNode {
+  return baseParse(template);
 }
