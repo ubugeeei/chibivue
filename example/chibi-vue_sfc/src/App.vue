ffc2043bd91f06ccc517d9dadfee442cb77a7ca5
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 20:55:00 +0900

    fmt

--- example/chibi-vue_sfc/src/App.vue
+++ example/chibi-vue_sfc/src/App.vue
@@ -2,19 +2,19 @@
 import { ref, computed } from "chibi-vue";
 
 export default {
-	setup() {
-		const count = ref(0);
-		const countDouble = computed(() => count.value * 2);
-		const message = ref("Hello chibi-vue!");
-		const changeMessage = () => {
-			count.value++;
-			message.value = `Hello chibi-vue! ${count.value} * 2 = ${countDouble.value}`;
-		};
-		return { message, changeMessage };
-	},
+  setup() {
+    const count = ref(0);
+    const countDouble = computed(() => count.value * 2);
+    const message = ref("Hello chibi-vue!");
+    const changeMessage = () => {
+      count.value++;
+      message.value = `Hello chibi-vue! ${count.value} * 2 = ${countDouble.value}`;
+    };
+    return { message, changeMessage };
+  },
 };
 </script>
 
 <template>
-	<div @click="changeMessage">{{ message }}</div>
+  <div @click="changeMessage">{{ message }}</div>
 </template>
