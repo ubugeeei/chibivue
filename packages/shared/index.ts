2cdea2b1f6762aa14c3b574c7c226a741a50e809
Ubugeeei <ubuge1122@gamil.com>
2023-03-12 10:58:41 +0900

    feat: test shared

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -44,7 +44,134 @@ export const toHandlerKey = (str: string) =>
  */
 if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest;
-  it("should work", () => {
-    expect(1).toBe(1);
+  it("unit test: isOn -> true", () => {
+    expect(isOn("onClick")).toBe(true);
+    expect(isOn("on-click")).toBe(true);
+    expect(isOn("on_click")).toBe(true);
+  });
+  it("unit test: isOn -> false", () => {
+    expect(isOn("onclick")).toBe(false);
+    expect(isOn("click")).toBe(false);
+  });
+
+  it("unit test: hasOwn -> true", () => {
+    expect(hasOwn({ a: 1 }, "a")).toBe(true);
+  });
+  it("unit test: hasOwn -> false", () => {
+    expect(hasOwn({ a: 1 }, "b")).toBe(false);
+    expect(hasOwn([], "push")).toBe(false);
+  });
+
+  it("unit test: isArray -> true", () => {
+    expect(isArray([])).toBe(true);
+    expect(isArray([1, 2, 3])).toBe(true);
+  });
+  it("unit test: isArray -> false", () => {
+    expect(isArray({})).toBe(false);
+    expect(isArray(null)).toBe(false);
+    expect(isArray(undefined)).toBe(false);
+  });
+
+  it("unit test: isMap -> true", () => {
+    expect(isMap(new Map())).toBe(true);
+  });
+  it("unit test: isMap -> false", () => {
+    expect(isMap(new Set())).toBe(false);
+    expect(isMap([])).toBe(false);
+    expect(isMap({})).toBe(false);
+  });
+
+  it("unit test: isSet -> true", () => {
+    expect(isSet(new Set())).toBe(true);
+  });
+  it("unit test: isSet -> false", () => {
+    expect(isSet(new Map())).toBe(false);
+    expect(isSet([])).toBe(false);
+    expect(isSet({})).toBe(false);
+  });
+
+  it("unit test: isFunction -> true", () => {
+    expect(isFunction(() => {})).toBe(true);
+    expect(isFunction(function f() {})).toBe(true);
+  });
+  it("unit test: isFunction -> false", () => {
+    expect(isFunction([])).toBe(false);
+    expect(isFunction({})).toBe(false);
+    expect(isFunction(null)).toBe(false);
+    expect(isFunction(undefined)).toBe(false);
+  });
+
+  it("unit test: isString -> true", () => {
+    expect(isString("")).toBe(true);
+    expect(isString("foo")).toBe(true);
+  });
+  it("unit test: isString -> false", () => {
+    expect(isString(1)).toBe(false);
+    expect(isString([])).toBe(false);
+    expect(isString({})).toBe(false);
+    expect(isString(null)).toBe(false);
+    expect(isString(undefined)).toBe(false);
+  });
+
+  it("unit test: isSymbol -> true", () => {
+    expect(isSymbol(Symbol())).toBe(true);
+    expect(isSymbol(Symbol("a"))).toBe(true);
+  });
+  it("unit test: isSymbol -> false", () => {
+    expect(isSymbol(1)).toBe(false);
+    expect(isSymbol("")).toBe(false);
+    expect(isSymbol([])).toBe(false);
+    expect(isSymbol({})).toBe(false);
+    expect(isSymbol(null)).toBe(false);
+    expect(isSymbol(undefined)).toBe(false);
+  });
+
+  it("unit test: isObject -> true", () => {
+    expect(isObject({})).toBe(true);
+    expect(isObject([])).toBe(true);
+    expect(isObject(new Map())).toBe(true);
+    expect(isObject(new Set())).toBe(true);
+  });
+  it("unit test: isObject -> false", () => {
+    expect(isObject(1)).toBe(false);
+    expect(isObject("")).toBe(false);
+    expect(isObject(null)).toBe(false);
+    expect(isObject(undefined)).toBe(false);
+  });
+
+  it("unit test: toTypeString", () => {
+    expect(toTypeString({})).toBe("[object Object]");
+    expect(toTypeString([])).toBe("[object Array]");
+    expect(toTypeString(new Map())).toBe("[object Map]");
+    expect(toTypeString(new Set())).toBe("[object Set]");
+    expect(toTypeString(1)).toBe("[object Number]");
+    expect(toTypeString("")).toBe("[object String]");
+    expect(toTypeString(null)).toBe("[object Null]");
+    expect(toTypeString(undefined)).toBe("[object Undefined]");
+  });
+
+  it("unit test: isPlainObject -> true", () => {
+    expect(isPlainObject({})).toBe(true);
+    expect(isPlainObject({ a: 1 })).toBe(true);
+  });
+  it("unit test: isPlainObject -> false", () => {
+    expect(isPlainObject([])).toBe(false);
+    expect(isPlainObject(new Map())).toBe(false);
+    expect(isPlainObject(new Set())).toBe(false);
+    expect(isPlainObject(1)).toBe(false);
+    expect(isPlainObject("")).toBe(false);
+    expect(isPlainObject(null)).toBe(false);
+    expect(isPlainObject(undefined)).toBe(false);
+  });
+
+  it("unit test: capitalize", () => {
+    expect(capitalize("")).toBe("");
+    expect(capitalize("a")).toBe("A");
+    expect(capitalize("ab")).toBe("Ab");
+  });
+
+  it("unit test: toHandlerKey", () => {
+    expect(toHandlerKey("")).toBe("");
+    expect(toHandlerKey("click")).toBe("onClick");
   });
 }
