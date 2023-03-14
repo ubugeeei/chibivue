f56f1a7c271642ef492a0135a959e641f7932775
Ubugeeei <ubuge1122@gamil.com>
2023-03-14 23:19:49 +0900

    feat: compileTemplate

--- packages/compiler-sfc/compileTemplate.ts
+++ packages/compiler-sfc/compileTemplate.ts
@@ -1,6 +1,26 @@
 import { CodegenResult, ParserOptions, RootNode } from "../compiler-core";
+import * as CompilerDOM from "../compiler-dom";
 
 export interface TemplateCompiler {
   compile(template: string): CodegenResult;
   parse(template: string, options: ParserOptions): RootNode;
+}
+
+export interface SFCTemplateCompileResults {
+  code: string;
+  source: string;
+  ast?: RootNode;
+}
+
+export interface SFCTemplateCompileOptions {
+  source: string;
+  compiler?: TemplateCompiler;
+}
+
+export function compileTemplate({
+  source,
+  compiler = CompilerDOM,
+}: SFCTemplateCompileOptions): SFCTemplateCompileResults {
+  let { code, ast } = compiler.compile(source);
+  return { code, ast, source };
 }
--- packages/compiler-sfc/index.ts
+++ packages/compiler-sfc/index.ts
@@ -1,3 +1,4 @@
 export * from "./parse";
 export { compileScript } from "./compileScript";
+export { compileTemplate } from "./compileTemplate";
 export { rewriteDefault } from "./rewriteDefault";
