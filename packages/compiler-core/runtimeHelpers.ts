07ed46f9649dd67fc6569276960899887e75391e
Ubugeeei <ubuge1122@gamil.com>
2023-03-19 01:20:45 +0900

    feat: renderList & Fragment

--- packages/compiler-core/runtimeHelpers.ts
+++ packages/compiler-core/runtimeHelpers.ts
@@ -1,11 +1,15 @@
+export const FRAGMENT = Symbol();
 export const CREATE_VNODE = Symbol();
 export const CREATE_ELEMENT_VNODE = Symbol();
+export const RENDER_LIST = Symbol();
 export const TO_DISPLAY_STRING = Symbol();
 export const TO_HANDLER_KEY = Symbol();
 
 export const helperNameMap: Record<symbol, string> = {
+  [FRAGMENT]: `Fragment`,
   [CREATE_VNODE]: `createVNode`,
   [CREATE_ELEMENT_VNODE]: `createElementVNode`,
   [TO_DISPLAY_STRING]: `toDisplayString`,
   [TO_HANDLER_KEY]: `toHandlerKey`,
+  [RENDER_LIST]: `renderList`,
 };
