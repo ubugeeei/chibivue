cd7eaf68ec455fd2a009f70c7de1e510a9c83918
Ubugeeei <ubuge1122@gamil.com>
2023-03-22 22:30:31 +0900

    feat: patchAttr

--- packages/runtime-dom/patchProp.ts
+++ packages/runtime-dom/patchProp.ts
@@ -2,6 +2,7 @@ import { isOn } from "../shared";
 import { patchEvent } from "./modules/events";
 import { type RendererOptions } from "../runtime-core";
 import { patchDOMProp } from "./modules/props";
+import { patchAttr } from "./modules/attrs";
 
 type DOMRendererOptions = RendererOptions<Node, Element>;
 
@@ -17,7 +18,9 @@ export const patchProp: DOMRendererOptions["patchProp"] = (
     // TODO: patch style
   } else if (isOn(key)) {
     patchEvent(el, key, nextValue);
-  } else {
+  } else if (!["type", "placeholder"].includes(key)) {
     patchDOMProp(el, key, nextValue);
+  } else {
+    patchAttr(el, key, nextValue);
   }
 };
