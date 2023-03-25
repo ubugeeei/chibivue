27740817d39428e629e99461cefaf10618ac1d7e
Ubugeeei <ubuge1122@gmail.com>
2023-03-25 23:50:20 +0900

    add store example

---
+++ example/chibi-vue_store/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import { useCounterStore } from "./store/count.store";
+
+export default {
+  setup() {
+    const counterStore = useCounterStore();
+    const counterStore2 = useCounterStore(); // point same store
+    return { counterStore, counterStore2 };
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Counter Store</h1>
+    <p>Count: {{ counterStore2.count }}</p>
+    <button @click="counterStore.increment">Increment</button>
+    <button @click="counterStore.reset">reset</button>
+  </div>
+</template>
