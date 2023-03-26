632a148401ca215878f1bb4ff3de44f0f431176b
Ubugeeei <ubuge1122@gmail.com>
2023-03-26 15:48:32 +0900

    feat: setCurrentRenderingInstance

--- packages/runtime-core/componentRenderUtils.ts
+++ packages/runtime-core/componentRenderUtils.ts
@@ -1,4 +1,5 @@
 import { type ComponentInternalInstance } from "./component";
+import { setCurrentRenderingInstance } from "./componentRenderContext";
 import { normalizeVNode, type VNode } from "./vnode";
 
 export function renderComponentRoot(
@@ -6,5 +7,6 @@ export function renderComponentRoot(
 ): VNode {
   const { proxy, render, data, ctx } = instance;
   const result = normalizeVNode(render!.call(proxy, proxy!, data, ctx));
+  setCurrentRenderingInstance(instance);
   return result;
 }
