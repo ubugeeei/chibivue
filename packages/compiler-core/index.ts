cba5c240eb6ee857915b3509709548a81fd0e577
Ubugeeei <ubuge1122@gmail.com>
2023-03-26 13:53:34 +0900

    feat: find props

--- packages/compiler-core/index.ts
+++ packages/compiler-core/index.ts
@@ -2,6 +2,11 @@ export { baseCompile } from "./compile";
 
 export * from "./ast";
 export * from "./options";
+
 export * from "./transform";
+export { transformModel } from "./transforms/vModel";
+
 export * from "./codegen";
 export * from "./compile";
+
+export * from "./utils";
