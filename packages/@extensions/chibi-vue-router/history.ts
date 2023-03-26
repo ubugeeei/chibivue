2239519f26ab99667f2543fb5d4314840f64415b
Ubugeeei <ubuge1122@gmail.com>
2023-03-26 22:14:34 +0900

    faet: createWebHistory

--- packages/@extensions/chibi-vue-router/history.ts
+++ packages/@extensions/chibi-vue-router/history.ts
@@ -4,3 +4,18 @@ export interface RouterHistory {
   replace(to: string): void;
   go(delta: number, triggerListeners?: boolean): void;
 }
+
+export const createWebHistory = (): RouterHistory => {
+  return {
+    location: window.location.pathname,
+    push(to: string) {
+      window.history.pushState({}, "", to);
+    },
+    replace(to: string) {
+      window.history.replaceState({}, "", to);
+    },
+    go(delta: number, triggerListeners?: boolean) {
+      window.history.go(delta);
+    },
+  };
+};
