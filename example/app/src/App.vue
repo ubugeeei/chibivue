c92b6e66f3610cebfce5dd4bb7aa12ee3373f461
Ubugeeei <ubuge1122@gmail.com>
2023-04-02 01:39:19 +0900

    styling example

--- example/app/src/App.vue
+++ example/app/src/App.vue
@@ -1,29 +1,51 @@
 <script setup>
 import { useRouter } from "chibi-vue-router";
 const router = useRouter();
+const buttonStyle =
+  "border: none; padding: 8px; width: 120px; background: #92b5a9; color: #fff; font-weight: bold; border-radius: 4px;";
 </script>
 
 <template>
   <div>
-    <nav>
-      <a @click="router.push('/')">Top</a>
-      <span>|</span>
-      <a @click="router.push('/state')">state</a>
-      <span>|</span>
-      <a @click="router.push('/directive')">directive</a>
-      <span>|</span>
-      <a @click="router.push('/props-emits')">props/emit</a>
-      <span>|</span>
-      <a @click="router.push('/compiler-macro')">compiler macro</a>
-      <span>|</span>
-      <a @click="router.push('/options-api')">options api</a>
-      <span>|</span>
-      <a @click="router.push('/inline')">inline</a>
-      <span>|</span>
-      <a @click="router.push('/store-counter')">store</a>
-      <span>|</span>
-      <a @click="router.push('/todo-app')">todo app</a>
-    </nav>
-    <RouterView />
+    <header>
+      <nav
+        style="
+          background: #1c273e;
+          height: 55px;
+          padding-top: 25px;
+          padding-left: 30px;
+        "
+      >
+        <button :style="buttonStyle" @click="router.push('/')">Top</button>
+        <button :style="buttonStyle" @click="router.push('/state')">
+          state
+        </button>
+        <button :style="buttonStyle" @click="router.push('/directive')">
+          directive
+        </button>
+        <button :style="buttonStyle" @click="router.push('/props-emits')">
+          props/emit
+        </button>
+        <button :style="buttonStyle" @click="router.push('/compiler-macro')">
+          compiler macro
+        </button>
+        <button :style="buttonStyle" @click="router.push('/options-api')">
+          options api
+        </button>
+        <button :style="buttonStyle" @click="router.push('/inline')">
+          inline
+        </button>
+        <button :style="buttonStyle" @click="router.push('/store-counter')">
+          store
+        </button>
+        <button :style="buttonStyle" @click="router.push('/todo-app')">
+          todo app
+        </button>
+      </nav>
+    </header>
+
+    <div style="background: #efeeea; padding: 20px;  height: 85vh;">
+      <RouterView />
+    </div>
   </div>
 </template>
