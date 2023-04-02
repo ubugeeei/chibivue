d48f3cda9b7eeb82a798d254644a26854a0edc2b
Ubugeeei <ubuge1122@gmail.com>
2023-04-03 00:05:01 +0900

    feat: load virtual style module

--- packages/@extensions/vite-plugin-chibi-vue/index.ts
+++ packages/@extensions/vite-plugin-chibi-vue/index.ts
@@ -3,6 +3,8 @@ import * as _compiler from "../../compiler-sfc";
 import type { Plugin } from "vite";
 import { createFilter } from "vite";
 import { transformMain } from "./main";
+import { parseChibiVueRequest } from "./utils/query";
+import { getDescriptor } from "./utils/descriptorCache";
 
 export interface ResolvedOptions {
   compiler: typeof _compiler;
@@ -18,6 +20,20 @@ export default function chibiVuePlugin(): Plugin {
 
   return {
     name: "vite:chibi-vue",
+
+    // virtual modules
+    resolveId(id) {
+      if (parseChibiVueRequest(id).query.chibiVue) return id;
+    },
+    load(id) {
+      const { filename, query } = parseChibiVueRequest(id);
+      if (query.chibiVue) {
+        const descriptor = getDescriptor(filename, options)!;
+        if (query.type === "style")
+          return { code: descriptor.styles[query.index!].content };
+      }
+    },
+
     transform(code, id, _) {
       if (!filter(id)) return;
       return transformMain(code, id, options);
