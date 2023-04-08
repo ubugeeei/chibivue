60ab71e7caa8c9ca3d4e16e730239eb32a8eb061
Ubugeeei <ubuge1122@gmail.com>
2023-04-08 20:07:38 +0900

    chap_code: createApp

---
+++ books/chapter_codes/02-2_create_app/packages/runtime-core/index.ts
@@ -0,0 +1,5 @@
+export type { App, CreateAppFunction } from "./apiCreateApp";
+export { createAppAPI } from "./apiCreateApp";
+
+export type { RendererOptions } from "./renderer";
+export { createRenderer } from "./renderer";
--- books/chapter_codes/02_create_app/packages/index.ts
+++ books/chapter_codes/02_create_app/packages/index.ts
@@ -1,3 +1,18 @@
-export const helloChibivue = () => {
-  console.log("Hello chibivue!");
+export type Options = {
+  render: () => string;
+};
+
+export type App = {
+  mount: (selector: string) => void;
+};
+
+export const createApp = (options: Options): App => {
+  return {
+    mount: (selector) => {
+      const root = document.querySelector(selector);
+      if (root) {
+        root.innerHTML = options.render();
+      }
+    },
+  };
 };
