91ab2133733d4d164e2db741b649928499b014ef
Ubugeeei <ubuge1122@gmail.com>
2023-04-18 21:30:12 +0900

    tweak: playground props

--- books/chapter_codes/06-2_mininum_component_system/examples/playground/src/main.ts
+++ books/chapter_codes/06-2_mininum_component_system/examples/playground/src/main.ts
@@ -6,7 +6,7 @@ const CounterComponent = {
     const increment = () => state.count++;
 
     return () =>
-      h("div", { id: "my-app" }, [
+      h("div", {}, [
         h("p", {}, [`count: ${state.count}`]),
         h("button", { onClick: increment }, ["increment"]),
       ]);
