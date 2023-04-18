fe21786154f2e939e076b65143980589d4ee7738
Ubugeeei <ubuge1122@gmail.com>
2023-04-18 20:20:34 +0900

    cp

---
+++ books/chapter_codes/06-2_mininum_component_system/examples/playground/src/main.ts
@@ -0,0 +1,27 @@
+import { createApp, h, reactive } from "chibivue";
+
+const CounterComponent = {
+  setup() {
+    const state = reactive({ count: 0 });
+    const increment = () => state.count++;
+
+    return () =>
+      h("div", { id: "my-app" }, [
+        h("p", {}, [`count: ${state.count}`]),
+        h("button", { onClick: increment }, ["increment"]),
+      ]);
+  },
+};
+
+const app = createApp({
+  setup: () => {
+    return () =>
+      h("div", { id: "my-app" }, [
+        h(CounterComponent, {}, []),
+        h(CounterComponent, {}, []),
+        h(CounterComponent, {}, []),
+      ]);
+  },
+});
+
+app.mount("#app");
