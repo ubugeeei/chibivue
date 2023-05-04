c7dfe59cf1f4540f0edd3a9fdd3e22a3202cfd51
Ubugeeei <ubuge1122@gmail.com>
2023-05-04 23:34:14 +0900

    chap_code: vite sample plugin

---
+++ books/chapter_codes/08_mininum_sfc_compiler/plugin-sample/src/plugin.sample.js
@@ -0,0 +1,13 @@
+function fizzbuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    i % 3 === 0 && i % 5 === 0
+      ? console.log("fizzbuzz")
+      : i % 3 === 0
+      ? console.log("fizz")
+      : i % 5 === 0
+      ? console.log("buzz")
+      : console.log(i);
+  }
+}
+
+fizzbuzz(Math.floor(Math.random() * 100) + 1);
