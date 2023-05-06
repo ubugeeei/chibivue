acb479d87b777d5c2cce62f213ed3e941b06f566
Ubugeeei <ubuge1122@gmail.com>
2023-05-06 22:35:00 +0900

    chap_code: fix vite error

--- books/chapter_codes/08-2_mininum_sfc_compiler/packages/@extensions/vite-plugin-chibivue/index.ts
+++ books/chapter_codes/08-2_mininum_sfc_compiler/packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,11 +1,15 @@
 import type { Plugin } from "vite";
+import { createFilter } from "vite";
 
 export default function vitePluginChibivue(): Plugin {
+  const filter = createFilter(/\.vue$/);
+
   return {
     name: "vite:chibivue",
 
     transform(code, id) {
-      return { code };
+      if (!filter(id)) return;
+      return { code: `export default {}` };
     },
   };
 }
