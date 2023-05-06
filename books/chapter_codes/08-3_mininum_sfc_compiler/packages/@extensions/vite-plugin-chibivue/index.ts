1f9cdd054cda465b52792e3961b194327342929e
Ubugeeei <ubuge1122@gmail.com>
2023-05-07 00:35:50 +0900

    cp

---
+++ books/chapter_codes/08-3_mininum_sfc_compiler/packages/@extensions/vite-plugin-chibivue/index.ts
@@ -0,0 +1,21 @@
+import type { Plugin } from "vite";
+import { createFilter } from "vite";
+import { parse } from "../../compiler-sfc";
+
+export default function vitePluginChibivue(): Plugin {
+  const filter = createFilter(/\.vue$/);
+
+  return {
+    name: "vite:chibivue",
+
+    transform(code, id) {
+      if (!filter(id)) return;
+      const { descriptor } = parse(code, { filename: id });
+      console.log(
+        "🚀 ~ file: index.ts:14 ~ transform ~ descriptor:",
+        descriptor
+      );
+      return { code: `export default {}` };
+    },
+  };
+}
