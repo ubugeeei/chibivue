d07aa17ace28de2c04826ea4739e7b2bbba044b8
Ubugeeei <ubuge1122@gmail.com>
2023-05-08 22:13:12 +0900

    chap_code: load virtual css module

--- books/chapter_codes/08-4_mininum_sfc_compiler/packages/@extensions/vite-plugin-chibivue/index.ts
+++ books/chapter_codes/08-4_mininum_sfc_compiler/packages/@extensions/vite-plugin-chibivue/index.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import type { Plugin } from "vite";
 import { createFilter } from "vite";
 import { parse, rewriteDefault } from "../../compiler-sfc";
@@ -8,12 +9,26 @@ export default function vitePluginChibivue(): Plugin {
 
   return {
     name: "vite:chibivue",
+    resolveId(id) {
+      if (id.match(/\.vue\.css$/)) return id;
+    },
+
+    load(id) {
+      if (id.match(/\.vue\.css$/)) {
+        const filename = id.replace(/\.css$/, "");
+        const content = fs.readFileSync("." + filename, "utf-8");
+        const { descriptor } = parse(content, { filename });
+        const styles = descriptor.styles.map((it) => it.content).join("\n");
+        return { code: styles };
+      }
+    },
 
     transform(code, id) {
       if (!filter(id)) return;
 
       const outputs = [];
       outputs.push("import * as ChibiVue from 'chibivue'");
+      outputs.push(`import '${id}.css'`);
 
       const { descriptor } = parse(code, { filename: id });
 
