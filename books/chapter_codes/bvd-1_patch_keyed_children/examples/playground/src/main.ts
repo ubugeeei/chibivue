a4463ae7eddc05c8d1dbdb6e96af101baad04b9c
Ubugeeei <ubuge1122@gmail.com>
2023-05-14 19:45:44 +0900

    chapter diagram

--- books/chapter_codes/bvd-1_patch_keyed_children/examples/playground/src/main.ts
+++ books/chapter_codes/bvd-1_patch_keyed_children/examples/playground/src/main.ts
@@ -1,6 +1,22 @@
-import { createApp } from "chibivue";
-import App from "./App.vue";
+import { createApp, h, reactive } from "chibivue";
 
-const app = createApp(App);
+const app = createApp({
+  setup() {
+    const state = reactive({ list: ["a", "b", "c", "d"] });
+    const updateList = () => {
+      state.list = ["e", "f", "g"];
+    };
+
+    return () =>
+      h("div", { id: "app" }, [
+        h(
+          "ul",
+          {},
+          state.list.map((item) => h("li", {}, [item]))
+        ),
+        h("button", { onClick: updateList }, ["update"]),
+      ]);
+  },
+});
 
 app.mount("#app");
