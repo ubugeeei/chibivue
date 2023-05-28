180fa32869e774f7f1be3579de429853d1385230
Ubugeeei <ubuge1122@gmail.com>
2023-05-28 22:29:08 +0900

    cp

---
+++ books/chapter_codes/bvd-2_bit_flags/packages/runtime-core/vnode.ts
@@ -0,0 +1,54 @@
+import { ComponentInternalInstance } from "./component";
+
+export type VNodeTypes = string | typeof Text | object;
+
+export const Text = Symbol();
+
+export interface VNode<HostNode = any> {
+  type: VNodeTypes;
+  props: VNodeProps | null;
+  children: VNodeNormalizedChildren;
+
+  el: HostNode | undefined;
+  key: string | number | symbol | null;
+
+  component: ComponentInternalInstance | null;
+}
+
+export interface VNodeProps {
+  [key: string]: any;
+}
+
+export type VNodeNormalizedChildren = string | VNodeArrayChildren;
+export type VNodeArrayChildren = Array<VNodeArrayChildren | VNodeChildAtom>;
+
+export type VNodeChild = VNodeChildAtom | VNodeArrayChildren;
+type VNodeChildAtom = VNode | string;
+
+export function createVNode(
+  type: VNodeTypes,
+  props: VNodeProps | null,
+  children: VNodeNormalizedChildren
+): VNode {
+  const vnode: VNode = {
+    type,
+    props,
+    children: children,
+    el: undefined,
+    key: props?.key ?? null,
+    component: null,
+  };
+  return vnode;
+}
+
+export function normalizeVNode(child: VNodeChild): VNode {
+  if (typeof child === "object") {
+    return { ...child } as VNode;
+  } else {
+    return createVNode(Text, null, String(child));
+  }
+}
+
+export function isSameVNodeType(n1: VNode, n2: VNode): boolean {
+  return n1.type === n2.type && n1.key === n2.key;
+}
