60c6f2bb07ff0009b8170d708c5eefca4a840d90
Ubugeeei <ubuge1122@gmail.com>
2023-06-11 19:40:42 +0900

    chap_code: add general function (isArray)

--- books/chapter_codes/bvd-5_other_props/packages/shared/general.ts
+++ books/chapter_codes/bvd-5_other_props/packages/shared/general.ts
@@ -1,3 +1,4 @@
+export const isArray = Array.isArray;
 export const isString = (val: unknown): val is string =>
   typeof val === "string";
 export const isObject = (val: unknown): val is Record<any, any> =>
