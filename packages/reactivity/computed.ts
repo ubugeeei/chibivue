dc8f33e0c557437d95826fae31c11c901a6cc2b0
Ubugeeei <ubuge1122@gmail.com>
2023-06-19 00:59:08 +0900

    feat: 💪 computed setter

--- packages/reactivity/computed.ts
+++ packages/reactivity/computed.ts
@@ -1,3 +1,4 @@
+import { isFunction } from "../shared";
 import { Dep } from "./dep";
 import { ReactiveEffect } from "./effect";
 import { Ref, trackRefValue, triggerRefValue } from "./ref";
@@ -10,7 +11,12 @@ interface WritableComputedRef<T> extends Ref<T> {
   readonly effect: ReactiveEffect<T>;
 }
 
-type ComputedGetter<T> = (...args: any[]) => T;
+export type ComputedGetter<T> = (...args: any[]) => T;
+export type ComputedSetter<T> = (v: T) => void;
+export interface WritableComputedOptions<T> {
+  get: ComputedGetter<T>;
+  set: ComputedSetter<T>;
+}
 
 export class ComputedRefImpl<T> {
   public dep?: Dep = undefined;
@@ -20,7 +26,10 @@ export class ComputedRefImpl<T> {
 
   public _dirty = true;
 
-  constructor(getter: ComputedGetter<T>) {
+  constructor(
+    getter: ComputedGetter<T>,
+    private readonly _setter: ComputedSetter<T>
+  ) {
     this.effect = new ReactiveEffect(getter, () => {
       if (!this._dirty) {
         this._dirty = true;
@@ -39,15 +48,31 @@ export class ComputedRefImpl<T> {
     return this._value;
   }
 
-  set value(_: T) {
-    // readonly
-    return;
+  set value(newValue: T) {
+    this._setter(newValue);
   }
 }
 
-export function computed<T>(getter: ComputedGetter<T>): ComputedRef<T>;
-export function computed<T>(getter: ComputedGetter<T>) {
-  return new ComputedRefImpl(getter);
+export function computed<T>(getterOrOptions: ComputedGetter<T>): ComputedRef<T>;
+export function computed<T>(
+  getterOrOptions: WritableComputedOptions<T>
+): Ref<T>;
+export function computed<T>(
+  getterOrOptions: ComputedGetter<T> | WritableComputedOptions<T>
+) {
+  let getter: ComputedGetter<T>;
+  let setter: ComputedSetter<T>;
+
+  const onlyGetter = isFunction(getterOrOptions);
+
+  if (onlyGetter) {
+    getter = getterOrOptions;
+    setter = () => {};
+  } else {
+    getter = getterOrOptions.get;
+    setter = getterOrOptions.set;
+  }
+  return new ComputedRefImpl(getter, setter) as any;
 }
 
 /**
