bb7836e809813543a8e5675a111576b8498b95c3
Ubugeeei <ubuge1122@gmail.com>
2023-06-18 22:04:34 +0900

    chapter: 📜 new chapter

---
+++ books/chapter_codes/210-brs-2-4_watch_api_extends/packages/runtime-core/apiWatch.ts
@@ -0,0 +1,26 @@
+import { ReactiveEffect } from "../reactivity";
+import { hasChanged } from "../shared";
+
+export type WatchEffect = (onCleanup: OnCleanup) => void;
+
+export type WatchSource<T = any> = () => T;
+
+type OnCleanup = (cleanupFn: () => void) => void;
+
+export function watch<T>(
+  source: WatchSource<T>,
+  cb: (newValue: T, oldValue: T) => void
+) {
+  let oldValue: T;
+  const getter = () => source();
+  const job = () => {
+    const newValue = getter();
+    if (hasChanged(newValue, oldValue)) {
+      cb(newValue, oldValue);
+      oldValue = newValue;
+    }
+  };
+
+  const effect = new ReactiveEffect(getter, job);
+  effect.run();
+}
---
+++ books/chapter_codes/210-brs-2-4_watch_api_extends/packages/shared/shapeFlags.ts
@@ -0,0 +1,4 @@
+export const enum ShapeFlags {
+  ELEMENT = 1,
+  COMPONENT = 1 << 2,
+}
