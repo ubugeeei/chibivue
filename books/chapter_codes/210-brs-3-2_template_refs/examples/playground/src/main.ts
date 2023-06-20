6edfc6027f17b458fc6be4e6e5db188b6d04f1ec
Ubugeeei <ubuge1122@gmail.com>
2023-06-20 09:23:06 +0900

    chapter: 📜 new

---
+++ books/chapter_codes/210-brs-3-2_template_refs/examples/playground/src/main.ts
@@ -0,0 +1,24 @@
+import { createApp, h, ref } from "chibivue";
+
+const app = createApp({
+  setup() {
+    const inputRef = ref<HTMLInputElement | null>(null);
+    const getRef = () => {
+      inputRef.value = document.getElementById(
+        "my-input"
+      ) as HTMLInputElement | null;
+    };
+    const focus = () => {
+      inputRef.value?.focus();
+    };
+
+    return () =>
+      h("div", {}, [
+        h("input", { id: "my-input" }, []),
+        h("button", { onClick: getRef }, ["getRef"]),
+        h("button", { onClick: focus }, ["focus"]),
+      ]);
+  },
+});
+
+app.mount("#app");
