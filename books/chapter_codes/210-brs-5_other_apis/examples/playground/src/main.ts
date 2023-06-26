91029878094e74dc5ddb54da2e06883208e5026b
Ubugeeei <ubuge1122@gmail.com>
2023-06-26 09:38:25 +0900

    chapter: 📜 new

---
+++ books/chapter_codes/210-brs-5_other_apis/examples/playground/src/main.ts
@@ -0,0 +1,43 @@
+import { createApp, h, reactive, watch } from "chibivue";
+
+const Component = {
+  setup() {
+    const state = reactive({ count: 0 });
+    const increment = () => {
+      state.count++;
+    };
+
+    const unwatch = watch(
+      () => state.count,
+      (newValue, oldValue, cleanup) => {
+        alert(`New value: ${newValue}, old value: ${oldValue}`);
+        cleanup(() => alert("Clean Up!"));
+      }
+    );
+
+    return () =>
+      h("div", {}, [
+        h("p", {}, [`count: ${state.count}`]),
+        h("button", { onClick: increment }, [`increment`]),
+        h("button", { onClick: unwatch }, [`unwatch`]),
+      ]);
+  },
+};
+
+const app = createApp({
+  setup() {
+    const isMounted = reactive({ value: false });
+    const toggle = () => {
+      isMounted.value = !isMounted.value;
+    };
+
+    return () =>
+      h("div", {}, [
+        h("p", {}, [`isMounted: ${isMounted.value}`]),
+        h("button", { onClick: toggle }, [`toggle`]),
+        isMounted.value ? h(Component, {}, []) : h("div", {}, []),
+      ]);
+  },
+});
+
+app.mount("#app");
---
+++ books/chapter_codes/210-brs-5_other_apis/packages/reactivity/baseHandler.ts
@@ -0,0 +1,22 @@
+import { isObject } from "../shared";
+import { track, trigger } from "./effect";
+import { reactive } from "./reactive";
+
+export const mutableHandlers: ProxyHandler<object> = {
+  get(target: object, key: string | symbol, receiver: object) {
+    track(target, key);
+
+    const res = Reflect.get(target, key, receiver);
+    if (isObject(res)) {
+      return reactive(res);
+    }
+
+    return res;
+  },
+
+  set(target: object, key: string | symbol, value: unknown, receiver: object) {
+    Reflect.set(target, key, value, receiver);
+    trigger(target, key);
+    return true;
+  },
+};
