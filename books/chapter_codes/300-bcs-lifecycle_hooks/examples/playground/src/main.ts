04d14ccca221b4943a71a017371504fb0346704b
Ubugeeei <ubuge1122@gmail.com>
2023-07-02 19:36:26 +0900

    chapter: 📜 new

---
+++ books/chapter_codes/300-bcs-lifecycle_hooks/examples/playground/src/main.ts
@@ -0,0 +1,48 @@
+import { createApp, customRef, h } from "chibivue";
+
+export function useDebouncedRef<T>(value: T, delay = 1000) {
+  let timeout: number;
+  return customRef((track, trigger) => {
+    return {
+      get() {
+        track();
+        return value;
+      },
+      set(newValue: T) {
+        window.clearTimeout(timeout);
+        timeout = window.setTimeout(() => {
+          value = newValue;
+          trigger();
+        }, delay);
+      },
+    };
+  });
+}
+
+const CustomRef = {
+  setup() {
+    const text = useDebouncedRef("hello");
+
+    return () =>
+      h("div", {}, [
+        h("h1", {}, ["CustomRef"]),
+        h("p", {}, [`${text.value}`]),
+        h(
+          "input",
+          {
+            value: text.value,
+            onInput: (e: any) => (text.value = e.target.value),
+          },
+          []
+        ),
+      ]);
+  },
+};
+
+const app = createApp({
+  setup() {
+    return () => h("div", {}, [h(CustomRef, {}, [])]);
+  },
+});
+
+app.mount("#app");
