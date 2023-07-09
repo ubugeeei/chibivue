27a8df69a267052305a4ba95ce88a74513c5e5b2
Ubugeeei <ubuge1122@gmail.com>
2023-07-09 16:06:33 +0900

    chapter: 📜 new

---
+++ books/chapter_codes/330-bcs-component_slot/packages/compiler-core/index.ts
@@ -0,0 +1,5 @@
+export * from "./codegen";
+export * from "./compile";
+export * from "./parse";
+export * from "./ast";
+export * from "./options";
---
+++ books/chapter_codes/330-bcs-component_slot/packages/runtime-core/componentOptions.ts
@@ -0,0 +1,14 @@
+import { SetupContext } from "./component";
+import { PropType } from "./componentProps";
+import { ComponentPublicInstance } from "./componentPublicInstance";
+import { VNode } from "./vnode";
+
+export type ComponentOptions<P = {}, B = {}> = {
+  props?: P;
+  setup?: (props: InferPropTypes<P>, ctx: SetupContext) => (() => VNode) | B;
+  render?: (ctx: ComponentPublicInstance<InferPropTypes<P>, B>) => VNode;
+  template?: string;
+};
+
+type InferPropTypes<T> = { [K in keyof T]: InferPropType<T[K]> };
+type InferPropType<T> = T extends { type: PropType<infer U> } ? U : never;
