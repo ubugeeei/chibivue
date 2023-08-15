58dfab6cbbde0afba038e1d64f1c2423b20c89c8
Ubugeeei <ubuge1122@gmail.com>
2023-08-15 15:11:28 +0900

    chapter: #34 📜 example

--- book/impls/50_basic_template_compiler/020_v_bind/examples/playground/src/main.ts
+++ book/impls/50_basic_template_compiler/020_v_bind/examples/playground/src/main.ts
@@ -2,21 +2,26 @@ import { createApp, defineComponent } from "chibivue";
 
 const App = defineComponent({
   setup() {
-    const bind = { id: "bind", class: "bind", style: "color: red" };
+    const bind = { id: "some-id", class: "some-class", style: "color: red" };
     return { count: 1, bind };
   },
 
-  template: `
-    <div>
-      <p v-bind:id="count"> hello </p>
-      <p :id="count * 2"> hello </p>
-      
-      <p v-bind:["style"]="bind.style"> hello </p>
-      <p :["style"]="bind.style"> hello </p>
+  template: `<div>
+  <p v-bind:id="count"> v-bind:id="count" </p>
+  <p :id="count * 2"> :id="count * 2" </p>
 
-      <p v-bind="bind"> hello </p>
-    </div>
-  `,
+  <p v-bind:["style"]="bind.style"> v-bind:["style"]="bind.style" </p>
+  <p :["style"]="bind.style"> :["style"]="bind.style" </p>
+
+  <p v-bind="bind"> v-bind="bind" </p>
+
+  <p :style="{ 'font-weight': 'bold' }"> :style="{ font-weight: 'bold' }" </p>
+  <p :style="'font-weight: bold;'"> :style="'font-weight: bold;'" </p>
+  <p :class="'my-class my-class2'"> :class="'my-class my-class2'" </p>
+  <p :class="['my-class']"> :class="['my-class']" </p>
+  <p :class="{ 'my-class': true }"> :class="{ 'my-class': true }" </p>
+  <p :class="{ 'my-class': false }"> :class="{ 'my-class': false }" </p>
+</div>`,
 });
 
 const app = createApp(App);
