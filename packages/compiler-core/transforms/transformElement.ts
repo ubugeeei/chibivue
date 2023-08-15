8fe93c42845dcc1072609c6cb5df5750f41b5ed9
Ubugeeei <ubuge1122@gmail.com>
2023-08-15 15:05:45 +0900

    feat: #34 💪 transform no args v-bind

--- packages/compiler-core/transforms/transformElement.ts
+++ packages/compiler-core/transforms/transformElement.ts
@@ -1,26 +1,39 @@
 import { isSymbol } from "../../shared";
 import {
   ArrayExpression,
+  CallExpression,
   ComponentNode,
   DirectiveArguments,
   DirectiveNode,
   ElementNode,
   ElementTypes,
+  ExpressionNode,
   NodeTypes,
   ObjectExpression,
   TemplateTextChildNode,
   VNodeCall,
   createArrayExpression,
+  createCallExpression,
   createObjectExpression,
   createObjectProperty,
   createSimpleExpression,
   createVNodeCall,
 } from "../ast";
+import {
+  MERGE_PROPS,
+  NORMALIZE_CLASS,
+  NORMALIZE_PROPS,
+  NORMALIZE_STYLE,
+} from "../runtimeHelpers";
 import { NodeTransform, TransformContext } from "../transform";
+import { isStaticExp } from "../utils";
 
 const directiveImportMap = new WeakMap<DirectiveNode, symbol>();
 
-export type PropsExpression = ObjectExpression;
+export type PropsExpression =
+  | ObjectExpression
+  | CallExpression
+  | ExpressionNode;
 
 export const transformElement: NodeTransform = (node, context) => {
   return function postTransformElement() {
@@ -94,15 +107,23 @@ export function buildProps(
   node: ElementNode,
   context: TransformContext
 ): { props: PropsExpression | undefined; directives: DirectiveNode[] } {
-  const { props } = node;
+  const { props, loc: elementLoc } = node;
   let properties: ObjectExpression["properties"] = [];
   const runtimeDirectives: DirectiveNode[] = [];
+  const mergeArgs: PropsExpression[] = [];
+
+  const pushMergeArg = (arg?: PropsExpression) => {
+    if (properties.length) {
+      mergeArgs.push(createObjectExpression(properties, elementLoc));
+      properties = [];
+    }
+    if (arg) mergeArgs.push(arg);
+  };
 
   for (let i = 0; i < props.length; i++) {
     const prop = props[i];
     if (prop.type === NodeTypes.ATTRIBUTE) {
       const { name, value } = prop;
-
       properties.push(
         createObjectProperty(
           createSimpleExpression(name, true),
@@ -111,7 +132,23 @@ export function buildProps(
       );
     } else {
       // directives
-      const { name } = prop;
+      const { name, arg, exp, loc } = prop;
+      const isVBind = name === "bind";
+      // const isVOn = name === "on"; // TODO:
+
+      // special case for v-bind and v-on with no argument
+      if (!arg && /*(*/ isVBind /* || isVOn) */) {
+        if (exp) {
+          if (isVBind /* || isVOn */) {
+            pushMergeArg();
+            mergeArgs.push(exp);
+          } else {
+            // TODO: v-on
+          }
+        }
+        continue;
+      }
+
       const directiveTransform = context.directiveTransforms[name];
       if (directiveTransform) {
         // has built-in directive transform.
@@ -128,8 +165,82 @@ export function buildProps(
   }
 
   let propsExpression: PropsExpression | undefined = undefined;
-  if (properties.length) {
+
+  // has v-bind="object" or v-on="object", wrap with mergeProps
+  if (mergeArgs.length) {
+    // close up any not-yet-merged props
+    pushMergeArg();
+    if (mergeArgs.length > 1) {
+      propsExpression = createCallExpression(
+        context.helper(MERGE_PROPS),
+        mergeArgs,
+        elementLoc
+      );
+    } else {
+      // single v-bind with nothing else - no need for a mergeProps call
+      propsExpression = mergeArgs[0];
+    }
+  } else if (properties.length) {
     propsExpression = createObjectExpression(properties);
+  }
+
+  if (propsExpression) {
+    switch (propsExpression.type) {
+      case NodeTypes.JS_OBJECT_EXPRESSION:
+        let classKeyIndex = -1;
+        let styleKeyIndex = -1;
+
+        for (let i = 0; i < propsExpression.properties.length; i++) {
+          const key = propsExpression.properties[i].key;
+          if (isStaticExp(key)) {
+            if (key.content === "class") {
+              classKeyIndex = i;
+            } else if (key.content === "style") {
+              styleKeyIndex = i;
+            }
+          }
+        }
+
+        const classProp = propsExpression.properties[classKeyIndex];
+        const styleProp = propsExpression.properties[styleKeyIndex];
+
+        if (classProp && !isStaticExp(classProp.value)) {
+          classProp.value = createCallExpression(
+            context.helper(NORMALIZE_CLASS),
+            [classProp.value]
+          );
+        }
+
+        if (
+          styleProp &&
+          ((styleProp.value.type === NodeTypes.SIMPLE_EXPRESSION &&
+            styleProp.value.content.trim()[0] === `[`) ||
+            styleProp.value.type === NodeTypes.JS_ARRAY_EXPRESSION)
+        ) {
+          styleProp.value = createCallExpression(
+            context.helper(NORMALIZE_STYLE),
+            [styleProp.value]
+          );
+        } else {
+          // dynamic key binding, wrap with `normalizeProps`
+          propsExpression = createCallExpression(
+            context.helper(NORMALIZE_PROPS),
+            [propsExpression]
+          );
+        }
+        break;
+
+      case NodeTypes.JS_CALL_EXPRESSION:
+        // mergeProps call, do nothing
+        break;
+
+      default:
+        // single v-bind
+        propsExpression = createCallExpression(context.helper(NORMALIZE_PROPS), [
+          propsExpression,
+        ]);
+        break;
+    }
   }
 
   return {
