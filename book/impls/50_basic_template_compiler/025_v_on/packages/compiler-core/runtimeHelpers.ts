3680a76e435eba2da8f71b101503873bbc1fae5e
Ubugeeei <ubuge1122@gmail.com>
2023-08-15 22:15:32 +0900

    chapter: #35 📜 impl runtime helpers

---
+++ book/impls/50_basic_template_compiler/025_v_on/packages/compiler-core/runtimeHelpers.ts
@@ -0,0 +1,13 @@
+export const CREATE_VNODE = Symbol();
+export const MERGE_PROPS = Symbol();
+export const NORMALIZE_CLASS = Symbol();
+export const NORMALIZE_STYLE = Symbol();
+export const NORMALIZE_PROPS = Symbol();
+
+export const helperNameMap: Record<symbol, string> = {
+  [CREATE_VNODE]: "createVNode",
+  [MERGE_PROPS]: "mergeProps",
+  [NORMALIZE_CLASS]: "normalizeClass",
+  [NORMALIZE_STYLE]: "normalizeStyle",
+  [NORMALIZE_PROPS]: "normalizeProps",
+};
