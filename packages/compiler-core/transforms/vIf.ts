7540d0027f1d2ef04440e8d304c8c47ed6ae9cc8
Ubugeeei <ubuge1122@gmail.com>
2023-10-01 04:01:05 +0900

    refactor: #40 🛠️ remove dead code

--- packages/compiler-core/transforms/vIf.ts
+++ packages/compiler-core/transforms/vIf.ts
@@ -5,16 +5,11 @@ import {
   IfConditionalExpression,
   IfNode,
   NodeTypes,
-  SimpleExpressionNode,
   VNodeCall,
   createCallExpression,
   createConditionalExpression,
-  createObjectExpression,
-  createObjectProperty,
-  createSimpleExpression,
-  createVNodeCall,
 } from "../ast";
-import { CREATE_COMMENT, FRAGMENT } from "../runtimeHelpers";
+import { CREATE_COMMENT } from "../runtimeHelpers";
 import {
   TransformContext,
   createStructuralDirectiveTransform,
@@ -25,28 +20,16 @@ export const transformIf = createStructuralDirectiveTransform(
   /^(if|else|else-if)$/,
   (node, dir, context) => {
     return processIf(node, dir, context, (ifNode, branch, isRoot) => {
-      const siblings = context.parent!.children;
-      let i = siblings.indexOf(ifNode);
-      let key = 0;
-      while (i-- >= 0) {
-        const sibling = siblings[i];
-        if (sibling && sibling.type === NodeTypes.IF) {
-          key += sibling.branches.length;
-        }
-      }
-
       return () => {
         if (isRoot) {
           ifNode.codegenNode = createCodegenNodeForBranch(
             branch,
-            key,
             context
           ) as IfConditionalExpression;
         } else {
           const parentCondition = getParentCondition(ifNode.codegenNode!);
           parentCondition.alternate = createCodegenNodeForBranch(
             branch,
-            key + ifNode.branches.length - 1,
             context
           );
         }
@@ -65,14 +48,6 @@ export function processIf(
     isRoot: boolean
   ) => (() => void) | undefined
 ) {
-  if (
-    dir.name !== "else" &&
-    (!dir.exp || !(dir.exp as SimpleExpressionNode).content.trim())
-  ) {
-    const loc = dir.exp ? dir.exp.loc : node.loc;
-    dir.exp = createSimpleExpression(`true`, false, loc);
-  }
-
   if (dir.name === "if") {
     const branch = createIfBranch(node, dir);
     const ifNode: IfNode = {
@@ -104,24 +79,13 @@ export function processIf(
       }
 
       if (sibling && sibling.type === NodeTypes.IF) {
-        if (
-          dir.name === "else-if" &&
-          sibling.branches[sibling.branches.length - 1].condition === undefined
-        ) {
-          throw new Error("X_V_ELSE_NO_ADJACENT_IF"); // TODO: error handling
-        }
-
         context.removeNode();
         const branch = createIfBranch(node, dir);
-
         sibling.branches.push(branch);
         const onExit = processCodegen && processCodegen(sibling, branch, false);
         traverseNode(branch, context);
-        // call on exit
         if (onExit) onExit();
         context.currentNode = null;
-      } else {
-        throw new Error("X_V_ELSE_NO_ADJACENT_IF"); // TODO: error handling
       }
       break;
     }
@@ -134,53 +98,32 @@ function createIfBranch(node: ElementNode, dir: DirectiveNode): IfBranchNode {
     loc: node.loc,
     condition: dir.name === "else" ? undefined : dir.exp,
     children: [node],
-    // userKey: findProp(node, `key`),
   };
 }
 
 function createCodegenNodeForBranch(
   branch: IfBranchNode,
-  keyIndex: number,
   context: TransformContext
 ): IfConditionalExpression | VNodeCall {
   if (branch.condition) {
     return createConditionalExpression(
       branch.condition,
-      createChildrenCodegenNode(branch, keyIndex, context),
+      createChildrenCodegenNode(branch, context),
       createCallExpression(context.helper(CREATE_COMMENT), ['""', "true"])
     ) as IfConditionalExpression;
   } else {
-    return createChildrenCodegenNode(branch, keyIndex, context);
+    return createChildrenCodegenNode(branch, context);
   }
 }
 
 function createChildrenCodegenNode(
   branch: IfBranchNode,
-  keyIndex: number,
   context: TransformContext
 ): VNodeCall {
-  const { helper } = context;
-  const keyProperty = createObjectProperty(
-    `key`,
-    createSimpleExpression(`${keyIndex}`, false)
-  );
   const { children } = branch;
   const firstChild = children[0];
-  const needFragmentWrapper =
-    children.length !== 1 || firstChild.type !== NodeTypes.ELEMENT;
-  if (needFragmentWrapper) {
-    return createVNodeCall(
-      context,
-      helper(FRAGMENT),
-      createObjectExpression([keyProperty]),
-      children
-    );
-  } else {
-    const vnodeCall = (firstChild as ElementNode).codegenNode as VNodeCall;
-    // inject branch key
-    // injectProp(vnodeCall, keyProperty, context);
-    return vnodeCall;
-  }
+  const vnodeCall = (firstChild as ElementNode).codegenNode as VNodeCall;
+  return vnodeCall;
 }
 
 function getParentCondition(
