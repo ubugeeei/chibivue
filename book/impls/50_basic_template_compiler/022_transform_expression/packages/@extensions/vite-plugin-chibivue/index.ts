9b90ce66004a154f1f7655b37604657d2372964a
Ubugeeei <ubuge1122@gmail.com>
2023-10-01 05:49:06 +0900

    chapter: #145 📜 new

---
+++ book/impls/50_basic_template_compiler/022_transform_expression/packages/@extensions/vite-plugin-chibivue/index.ts
@@ -0,0 +1,53 @@
+import fs from "node:fs";
+import type { Plugin } from "vite";
+import { createFilter } from "vite";
+import { parse, rewriteDefault } from "../../compiler-sfc";
+import { compile } from "../../compiler-dom";
+
+export default function vitePluginChibivue(): Plugin {
+  const filter = createFilter(/\.vue$/);
+
+  return {
+    name: "vite:chibivue",
+    resolveId(id) {
+      if (id.match(/\.vue\.css$/)) return id;
+    },
+
+    load(id) {
+      if (id.match(/\.vue\.css$/)) {
+        const filename = id.replace(/\.css$/, "");
+        const content = fs.readFileSync("." + filename, "utf-8");
+        const { descriptor } = parse(content, { filename });
+        const styles = descriptor.styles.map((it) => it.content).join("\n");
+        return { code: styles };
+      }
+    },
+
+    transform(code, id) {
+      if (!filter(id)) return;
+
+      const outputs = [];
+      outputs.push("import * as ChibiVue from 'chibivue'");
+      outputs.push(`import '${id}.css'`);
+
+      const { descriptor } = parse(code, { filename: id });
+
+      const SFC_MAIN = "_sfc_main";
+      const scriptCode = rewriteDefault(
+        descriptor.script?.content ?? "",
+        SFC_MAIN
+      );
+      outputs.push(scriptCode);
+
+      const templateCode = compile(descriptor.template?.content ?? "", {
+        isBrowser: false,
+      });
+      outputs.push(templateCode);
+
+      outputs.push("\n");
+      outputs.push(`export default { ...${SFC_MAIN}, render }`);
+
+      return { code: outputs.join("\n") };
+    },
+  };
+}
---
+++ book/impls/50_basic_template_compiler/022_transform_expression/packages/compiler-core/transform.ts
@@ -0,0 +1,105 @@
+import { isArray, isString } from "../shared";
+import {
+  NodeTypes,
+  RootNode,
+  TemplateChildNode,
+  ParentNode,
+  Property,
+  ElementNode,
+  DirectiveNode,
+} from "./ast";
+import { TransformOptions } from "./options";
+
+export type NodeTransform = (
+  node: RootNode | TemplateChildNode,
+  context: TransformContext
+) => void | (() => void) | (() => void)[];
+
+export type DirectiveTransform = (
+  dir: DirectiveNode,
+  node: ElementNode,
+  context: TransformContext
+) => DirectiveTransformResult;
+
+export interface DirectiveTransformResult {
+  props: Property[];
+}
+
+export interface TransformContext extends Required<TransformOptions> {
+  currentNode: RootNode | TemplateChildNode | null;
+  parent: ParentNode | null;
+  childIndex: number;
+}
+
+export function createTransformContext(
+  root: RootNode,
+  { nodeTransforms = [], directiveTransforms = {} }: TransformOptions
+): TransformContext {
+  const context: TransformContext = {
+    nodeTransforms,
+    directiveTransforms,
+    currentNode: root,
+    parent: null,
+    childIndex: 0,
+  };
+
+  return context;
+}
+
+export function transform(root: RootNode, options: TransformOptions) {
+  const context = createTransformContext(root, options);
+  traverseNode(root, context);
+}
+
+export function traverseNode(
+  node: RootNode | TemplateChildNode,
+  context: TransformContext
+) {
+  context.currentNode = node;
+
+  const { nodeTransforms } = context;
+  const exitFns = [];
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const onExit = nodeTransforms[i](node, context);
+    if (onExit) {
+      if (isArray(onExit)) {
+        exitFns.push(...onExit);
+      } else {
+        exitFns.push(onExit);
+      }
+    }
+    if (!context.currentNode) {
+      return;
+    } else {
+      node = context.currentNode;
+    }
+  }
+
+  switch (node.type) {
+    case NodeTypes.INTERPOLATION:
+      break;
+    case NodeTypes.ELEMENT:
+    case NodeTypes.ROOT:
+      traverseChildren(node, context);
+      break;
+  }
+
+  context.currentNode = node;
+  let i = exitFns.length;
+  while (i--) {
+    exitFns[i]();
+  }
+}
+
+export function traverseChildren(
+  parent: ParentNode,
+  context: TransformContext
+) {
+  for (let i = 0; i < parent.children.length; i++) {
+    const child = parent.children[i];
+    if (isString(child)) continue;
+    context.parent = parent;
+    context.childIndex = i;
+    traverseNode(child, context);
+  }
+}
