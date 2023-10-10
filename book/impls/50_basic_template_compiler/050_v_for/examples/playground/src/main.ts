fd166f62ad00960974e136796aee7155cc5eeaa5
Ubugeeei <ubuge1122@gmail.com>
2023-10-11 01:15:33 +0900

    chapter: #39 📜 sample

--- book/impls/50_basic_template_compiler/050_v_for/examples/playground/src/main.ts
+++ book/impls/50_basic_template_compiler/050_v_for/examples/playground/src/main.ts
@@ -1,23 +1,7 @@
-import { createApp, defineComponent, ref } from "chibivue";
+import { createApp } from "chibivue";
 
-const App = defineComponent({
-  setup() {
-    const n = ref(1);
-    const inc = () => {
-      n.value++;
-    };
-
-    return { n, inc };
-  },
-
-  template: `
-    <button @click="inc">inc</button>
-    <p v-if="n % 5 === 0 && n % 3 === 0">FizzBuzz</p>
-    <p v-else-if="n % 5 === 0">Buzz</p>
-    <p v-else-if="n % 3 === 0">Fizz</p>
-    <p v-else>{{ n }}</p>
-  `,
-});
+// @ts-ignore
+import App from "./App.vue";
 
 const app = createApp(App);
 
