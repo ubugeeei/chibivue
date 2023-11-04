3f715ab04326ad201e5e2965e996dab8f4325156
Ubugeeei <ubuge1122@gmail.com>
2023-11-05 04:25:48 +0900

    chapter: #30 📜 playground

--- book/impls/50_basic_template_compiler/060_resolve_components/examples/playground/src/App.vue
+++ book/impls/50_basic_template_compiler/060_resolve_components/examples/playground/src/App.vue
@@ -1,75 +1,15 @@
 <script>
-import { createApp, defineComponent, ref } from "chibivue";
+import Counter from "./components/Counter.vue";
 
-const genId = () => Math.random().toString(36).slice(2);
+import { defineComponent } from "chibivue";
 
-const FRUITS_FACTORIES = [
-  () => ({ id: genId(), name: "apple", color: "red" }),
-  () => ({ id: genId(), name: "banana", color: "yellow" }),
-  () => ({ id: genId(), name: "grape", color: "purple" }),
-];
-
-export default {
-  setup() {
-    const fruits = ref([...FRUITS_FACTORIES].map((f) => f()));
-    const addFruit = () => {
-      fruits.value.push(
-        FRUITS_FACTORIES[Math.floor(Math.random() * FRUITS_FACTORIES.length)]()
-      );
-    };
-    return { fruits, addFruit };
-  },
-};
+export default defineComponent({
+  components: { Counter },
+});
 </script>
 
 <template>
-  <button @click="addFruit">add fruits!</button>
-
-  <!-- basic -->
-  <ul>
-    <li v-for="fruit in fruits" :key="fruit.id">
-      <span :style="{ backgroundColor: fruit.color }">{{ fruit.name }}</span>
-    </li>
-  </ul>
-
-  <!-- indexed -->
-  <ul>
-    <li v-for="(fruit, i) in fruits" :key="fruit.id">
-      <span :style="{ backgroundColor: fruit.color }">{{ fruit.name }}</span>
-    </li>
-  </ul>
-
-  <!-- destructuring -->
-  <ul>
-    <li v-for="({ id, name, color }, i) in fruits" :key="id">
-      <span :style="{ backgroundColor: color }">{{ name }}</span>
-    </li>
-  </ul>
-
-  <!-- object -->
-  <ul>
-    <li v-for="(value, key, idx) in fruits[0]" :key="key">
-      [{{ idx }}] {{ key }}: {{ value }}
-    </li>
-  </ul>
-
-  <!-- range -->
-  <ul>
-    <li v-for="n in 10">{{ n }}</li>
-  </ul>
-
-  <!-- string -->
-  <ul>
-    <li v-for="c in 'hello'">{{ c }}</li>
-  </ul>
-
-  <!-- nested -->
-  <ul>
-    <li v-for="({ id, name, color }, i) in fruits" :key="id">
-      <span :style="{ backgroundColor: color }">
-        <span v-for="n in 3">{{ n }}</span>
-        {{ name }}</span
-      >
-    </li>
-  </ul>
+  <Counter />
+  <Counter />
+  <GlobalCounter />
 </template>
--- book/impls/50_basic_template_compiler/060_resolve_components/examples/playground/src/main.ts
+++ book/impls/50_basic_template_compiler/060_resolve_components/examples/playground/src/main.ts
@@ -1,8 +1,13 @@
 import { createApp } from "chibivue";
 
 // @ts-ignore
+import Counter from "./components/Counter.vue";
+
+// @ts-ignore
 import App from "./App.vue";
 
 const app = createApp(App);
+
+app.component("GlobalCounter", Counter);
 
 app.mount("#app");
