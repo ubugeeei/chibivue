3e17d884a77a3f02b6defb2e7dcc6752cc703749
ren-yamanashi <pinokionigg@icloud.com>
2023-12-29 23:21:35 +0900

    feat:💪 render mermaid #183

--- book/online-book/.vitepress/config/index.ts
+++ book/online-book/.vitepress/config/index.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "vitepress";
 import { sharedConfig } from "./shared.js";
 import { jaConfig } from "./ja";
 import { enConfig } from "./en.js";
+import { withMermaid } from "vitepress-plugin-mermaid";
+import { defineConfig } from "vitepress";
 
-export default defineConfig({
+export default (process.env.NODE_ENV === "production"
+  ? withMermaid
+  : defineConfig)({
   ...sharedConfig,
   locales: {
     root: { label: "Japanese", lang: "ja", link: "/", ...jaConfig },
