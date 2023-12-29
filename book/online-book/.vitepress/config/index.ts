5d06dc3489e6d12ca242caa12919f470ca319e5e
ren-yamanashi <pinokionigg@icloud.com>
2023-12-29 23:52:03 +0900

    feat:💪 add comment reason for countermeasure #183

--- book/online-book/.vitepress/config/index.ts
+++ book/online-book/.vitepress/config/index.ts
@@ -4,6 +4,9 @@ import { enConfig } from "./en.js";
 import { withMermaid } from "vitepress-plugin-mermaid";
 import { defineConfig } from "vitepress";
 
+
+// The same situation as the issue below is occurring, so mermaid is rendered only during production build.
+// https://github.com/iamkun/dayjs/issues/480
 export default (process.env.NODE_ENV === "production"
   ? withMermaid
   : defineConfig)({
