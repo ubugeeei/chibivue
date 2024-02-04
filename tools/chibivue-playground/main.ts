56bd03a10cf62ee104c7ba960053b10826645dbf
Ubugeeei <ubuge1122@gmail.com>
2024-02-04 20:39:38 +0900

    fix: #246 📌 dev setup

--- tools/chibivue-playground/main.ts
+++ tools/chibivue-playground/main.ts
@@ -5,7 +5,7 @@ const red = (s: string) => `\x1b[31m${s}\x1b[0m`
 const green = (s: string) => `\x1b[32m${s}\x1b[0m`
 const blue = (s: string) => `\x1b[34m${s}\x1b[0m`
 
-const targetDirPath = 'example/playground'
+const targetDirPath = 'examples/playground'
 
 // check if target path is empty
 if (!targetDirPath) {
